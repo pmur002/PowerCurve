e8e86b2f46d8fdc98f4f15e3a495d1b5069cbd96
Paul Murrell <paul@stat.auckland.ac.nz>
2017-02-02 07:27:01 +1300

    improvement and fine tuning of face example

--- face.R
+++ face.R
@@ -10,7 +10,7 @@ eyelash <- function(x, y, angle, flip=FALSE) {
     pushViewport(viewport(x, y, just=c("centre", "bottom"), angle=angle))
     grid.vwline(x=unit(.5, "native") - xoffset,
                 y=unit(c(0, 4, 6, 8), "mm"),
-                w=unit(3:0/4, "mm"), shape=1, endShape=0,
+                w=unit(3:0/8, "mm"), shape=1, endShape=0,
                 gp=gpar(fill="black"))
     ## grid.rect(.5, 0, .1, .1, just=c("centre", "bottom"))
     popViewport()
@@ -30,31 +30,63 @@ eyelid <- function(x, y, w, angle, flip=FALSE) {
     pushViewport(viewport(x, y, just=c("centre", "bottom"),
                           width=w, angle=angle))
     lid <- vwlineGrob(x=xx, y=yy,
-                      w=unit(c(0, 1, 2, 1, 0), "mm"), shape=1, endShape=0,
+                      w=unit(c(0, 1, 2, 1, 0)/2, "mm"), shape=1, endShape=0,
                       gp=gpar(fill="black"))
     grid.draw(lid)
     pts <- vwEdgePoints(lid, seq(.2, .8, .2), "lower")
     for (i in 1:4) {
         eyelash(pts$lower$x[i], pts$lower$y[i],
-                180*pts$lower$tangent[i]/pi, flip=flip)
+                180*pts$lower$tangent[i]/pi + 180, flip=flip)
     }
     ## grid.rect()
     popViewport()    
 }
 
-nose <- function(x, y) {
-    pushViewport(viewport(x, y, just=c("left", "bottom")))
-    grid.vwline(unit(c(-15, 15, 3, -3), "mm"),
-                unit(c(10, 10, -10, -10), "mm"),
-                unit(c(2, 2, 1, 1), "mm"),
-                open=FALSE, shape=1,
-                gp=gpar(fill="black"),
-                name="nose")
-    ## vwEdgePoints(grid.get("nose"), 0:3/3, debug=TRUE)
+lip <- function(x, y, angle, flip=FALSE) {
+    xoffset <- unit(c(0, 1, 4, 9)*2, "mm")
+    if (flip) {
+        xoffset <- -1*xoffset
+    } 
+    pushViewport(viewport(x, y, just=c("centre", "bottom"), angle=angle))
+    grid.vwline(x=unit(.5, "native") - xoffset,
+                y=unit(c(0, 4, 8, 12)*2, "mm"),
+                w=unit(3:0/8, "mm"), shape=1, endShape=0,
+                gp=gpar(fill="black"))
     popViewport()
 }
 
+nose <- function(x, y) {
+    pushViewport(viewport(x, y, just=c("left", "bottom")))
+    nosegrob <- vwlineGrob(unit(c(0, -15, 15), "mm"),
+                           unit(c(-10, 10, 10), "mm"),
+                           unit(c(1, 2, 2)/2, "mm"),
+                           open=FALSE, shape=.7,
+                           gp=gpar(fill="black"),
+                           name="nose")
+    grid.draw(nosegrob)
+    pts <- vwEdgePoints(nosegrob, c(.03, .97), "upper")
+    lip(pts$upper$x[1], pts$upper$y[1],
+        180*pts$upper$tangent[1]/pi, flip=TRUE)
+    lip(pts$upper$x[2], pts$upper$y[2],
+        180*pts$upper$tangent[2]/pi)
+    popViewport()
+}
+
+whiskers <- function(x, y) {
+    pushViewport(viewport(x, y, just=c("left", "bottom")))
+    grid.points(unit(runif(6, -1, 1)/2, "cm"),
+                unit(runif(6, -1, 1)/2, "cm"),
+                size=unit(2, "mm"), pch=16)
+    popViewport()
+}
+
+png("face.png", width=20, height=10, units="in", res=100, bg="transparent")
 grid.newpage()
-eyelid(.7, .7, .25, -20)
-eyelid(.3, .7, .25, 20, flip=TRUE)
+pushViewport(viewport(width=unit(7, "in"), height=unit(7, "in")))
+eyelid(.65, .7, .2, -20)
+eyelid(.35, .7, .2, 20, flip=TRUE)
 nose(.5, .5)
+whiskers(.35, .5)
+whiskers(.65, .5)
+dev.off()
+
